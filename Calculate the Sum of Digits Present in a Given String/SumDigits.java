96a0dd65a30ff6912ca2b2509904d34f528515f8
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2024-10-01 08:06:38 +0400

    Add files via upload

---
+++ Calculate the Sum of Digits Present in a Given String/SumDigits.java
@@ -0,0 +1,19 @@
+public class SumDigits {
+    public static void main(String[] args) {
+        System.out.println(sumDigits("aa1bc2d3"));
+        System.out.println(sumDigits("aa11b33"));
+        System.out.println(sumDigits("chocolate"));
+    }
+    public static int sumDigits(String s) {
+        int sum = 0;
+        for(char c : s.toCharArray()) {
+            // If the character is a digit, convert it to its integer value and add to sum
+            if(Character.isDigit(c)) {
+                sum += Character.getNumericValue(c);// Converts char to int and adds to sum
+            }
+        }
+
+
+        return sum;
+    }
+}
