03423a382280816b6b8ac34b5cbb965f40f552fd
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2024-10-21 12:07:11 +0400

    Add files via upload

---
+++ Update Toppings Map/ToppingModifier.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ToppingModifier {
+    public static void main(String[] args) {
+
+        Map<String, String> toppingMap = new HashMap<>();
+        toppingMap.put("ice cream", "peanuts");
+        toppingMap.put("pancake", "syrup");
+
+        topping1(toppingMap);
+
+        System.out.println(toppingMap);
+    }
+
+    public static void topping1(Map<String, String> map) {
+        // If "ice cream" is present, set its value to "cherry"
+        if (map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        }
+        // Always set "bread" to "butter"
+        map.put("bread", "butter"); 
+    }
+}
