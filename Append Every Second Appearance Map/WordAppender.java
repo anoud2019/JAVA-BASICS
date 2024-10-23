b9707d9c172ece60be6e1d3d7714b7b1d9fb1905
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2024-10-23 10:15:13 +0400

    Add files via upload

---
+++ Append Every Second Appearance Map/WordAppender.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordAppender {
+    public static void main(String[] args) {
+        String[] letters = {"a", "b", "c", "a", "s", "a", "a", "a", "a"};
+        wordAppend(letters);
+    }
+
+    public static void wordAppend(String[] strings) {
+        Map<String, Integer> map = new HashMap<>();
+        String result = "";
+
+        for (String s : strings) {
+            // If the string is already in the map, increment the count
+            if (map.containsKey(s)) {
+                int count = map.get(s);
+                map.put(s, count + 1);
+            } else {
+                // If it's a new string, add it to the map with an initial count of 1
+                map.put(s, 1);
+            }
+
+            // If the count of the string is even, append it to the result string
+            if (map.get(s) % 2 == 0) {
+                result += s;
+            }
+        }
+        System.out.println(result);
+    }
+}
