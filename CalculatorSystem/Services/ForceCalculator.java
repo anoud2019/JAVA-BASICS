d5d084a6f835fa49219459a37e6b103b4e60658b
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2024-11-20 07:07:00 +0400

    Update ForceCalculator.java

--- CalculatorSystem/Services/ForceCalculator.java
+++ CalculatorSystem/Services/ForceCalculator.java
@@ -6,22 +6,19 @@ import Interfaces.CalculatorInterface;
 import java.util.Scanner;
 
 public class ForceCalculator implements CalculatorInterface {
-    private PhysicsData physicsData;
 
-    public ForceCalculator(PhysicsData physicsData) {
-        this.physicsData = physicsData;
-    }
 
     @Override
     public void calculate() {
         Scanner scanner=new Scanner(System.in);
+        PhysicsData physicsData=new PhysicsData();
         System.out.println("Formula: (F = ma)");
         System.out.println("Enter Mass (m):");
         physicsData.setMass(scanner.nextDouble());
         System.out.println("Enter Acceleration (a):");
         physicsData.setAcceleration(scanner.nextDouble());
 
-        double findForce=physicsData.getMass()*physicsData.getAcceleration();
+        double findForce= physicsData.getMass()* physicsData.getAcceleration();
         System.out.println("The Force (F):"+findForce);
     }
 }
