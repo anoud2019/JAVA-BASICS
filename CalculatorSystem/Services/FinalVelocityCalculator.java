e741cda122197e150dbba0838da51094910ba79a
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2024-11-20 07:08:10 +0400

    Update FinalVelocityCalculator.java

--- CalculatorSystem/Services/FinalVelocityCalculator.java
+++ CalculatorSystem/Services/FinalVelocityCalculator.java
@@ -5,22 +5,21 @@ import Entities.MotionData;
 import Interfaces.CalculatorInterface;
 
 import java.util.Scanner;
+import java.util.UUID;
 
 public class FinalVelocityCalculator implements CalculatorInterface {
-    private MotionData motionData;
 
-    public FinalVelocityCalculator(MotionData motionData) {
-        this.motionData = motionData;
-    }
 
     @Override
     public void calculate() {
         Scanner s = new Scanner(System.in);
+        MotionData motionData=new MotionData();
         System.out.println("Enter ID:");
         motionData.setId(s.nextInt());
         s.nextLine();
         System.out.println("Enter Description:");
         motionData.setDescription(s.nextLine());
+
 
         System.out.println("Formula: v = u + at");
         System.out.print("Enter initial velocity (u): ");
@@ -34,8 +33,8 @@ public class FinalVelocityCalculator implements CalculatorInterface {
         double finalVelocity = motionData.getInitialVelocity() +
                 (motionData.getAcceleration() * motionData.getTime());
 
-       // System.out.println("ID: " + motionData.getId());
-       // System.out.println("Description: " + motionData.getDescription());
+        System.out.println("ID: " + motionData.getId());
+        System.out.println("Description: " + motionData.getDescription());
 
         System.out.println( "The final velocity (v) is: " + finalVelocity);
     }
