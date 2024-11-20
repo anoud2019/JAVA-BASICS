59654971cd8c1e9559a67b5b81b31a8afbf6e4f5
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2024-11-20 07:06:30 +0400

    Update SimpleInterestCalculator.java

--- CalculatorSystem/Services/SimpleInterestCalculator.java
+++ CalculatorSystem/Services/SimpleInterestCalculator.java
@@ -6,15 +6,11 @@ import Interfaces.CalculatorInterface;
 import java.util.Scanner;
 
 public class SimpleInterestCalculator implements CalculatorInterface {
-    private InterestData interestData;
-
-    public SimpleInterestCalculator(InterestData interestData) {
-        this.interestData = interestData;
-    }
 
     @Override
     public void calculate() {
         Scanner scanner=new Scanner(System.in);
+        InterestData interestData=new InterestData();
         System.out.println("Formula:(SI = P × R × T ÷ 100)");
         System.out.println("Enter principal(P):");
         interestData.setPrincipal(scanner.nextDouble());
