dd9b233bdc863f95af620f39c7d1491cd514fbe1
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2024-11-20 07:08:32 +0400

    Update DisplacementCalculator.java

--- CalculatorSystem/Services/DisplacementCalculator.java
+++ CalculatorSystem/Services/DisplacementCalculator.java
@@ -6,15 +6,11 @@ import Interfaces.CalculatorInterface;
 import java.util.Scanner;
 
 public class DisplacementCalculator implements CalculatorInterface {
-    private MotionData motionData;
-
-    public DisplacementCalculator(MotionData motionData) {
-        this.motionData = motionData;
-    }
 
     @Override
     public void calculate() {
         Scanner scanner=new Scanner(System.in);
+        MotionData motionData=new MotionData();
         System.out.println("Formula: (s = ut + ½at²)");
 
         System.out.println("Enter initialVelocity (u):");
@@ -24,7 +20,7 @@ public class DisplacementCalculator implements CalculatorInterface {
         System.out.println("Enter acceleration (a):");
         motionData.setAcceleration(scanner.nextDouble());
 
-        double findDisplacement = (motionData.getInitialVelocity() * motionData.getTime()) + (0.5 * motionData.getAcceleration() * Math.pow(motionData.getTime(), 2));
+        double findDisplacement = (motionData.getInitialVelocity()* motionData.getTime()) + (0.5 * motionData.getAcceleration() * Math.pow(motionData.getTime(), 2));
         System.out.println("The Displacement (s) is:" + " " + findDisplacement);
     }
 }
