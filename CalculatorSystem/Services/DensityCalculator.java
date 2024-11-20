2eab609d7673a891f41921bf585ed710284d059f
anoud2019 <167195206+anoud2019@users.noreply.github.com>
2024-11-20 07:09:02 +0400

    Update DensityCalculator.java

--- CalculatorSystem/Services/DensityCalculator.java
+++ CalculatorSystem/Services/DensityCalculator.java
@@ -6,23 +6,19 @@ import Interfaces.CalculatorInterface;
 import java.util.Scanner;
 
 public class DensityCalculator implements CalculatorInterface {
-    private PhysicsData physicsData;
 
-    public DensityCalculator(PhysicsData physicsData) {
-        this.physicsData = physicsData;
-    }
-
+    // Takes mass and volume as input by user, calculates density, and displays the result.
     @Override
     public void calculate() {
         Scanner s = new Scanner(System.in);
         System.out.println("Formula: Density = Mass ÷ Volume");
-
+        PhysicsData physicsData=new PhysicsData();
         System.out.print("Enter mass : ");
         physicsData.setMass(s.nextDouble());
         System.out.print("Enter volume : ");
-        double volume = s.nextDouble();
+        physicsData.setVolume(s.nextDouble());
 
-        double density = physicsData.getMass() / volume;
+        double density = physicsData.getMass() / physicsData.getVolume();
 
         System.out.println("The density is: " + density );
     }
